40adb34bab5136207c4a800a4a08845c546b3720
Alberto Perez <aperez@alea-soluciones.com>
2015-02-18 16:01:00 +0100

    Update license to all files

--- queries_service/queries_service_test.go
+++ queries_service/queries_service_test.go
@@ -1,3 +1,7 @@
+// Copyright 2015 The http2amqp Authors. All rights reserved.  Use of this
+// source code is governed by a MIT-style license that can be found in the
+// LICENSE file.
+
 package queries_service_test
 
 import (
