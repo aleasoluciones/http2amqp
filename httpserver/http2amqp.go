e026309bbe48abcbe0e32ce8e9d577f02a14657c
Eduardo Ferro Aldama <eduardo.ferro.aldama@gmail.com>
2015-08-18 11:48:46 +0200

    Added parameter for configure port and address to listen

--- httpserver/http2amqp.go
+++ httpserver/http2amqp.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	_ "net/http/pprof"
@@ -12,23 +13,19 @@ import (
 )
 
 func main() {
-	brokerUri, exchange, timeout := parseArgs()
-
-	http2amqpService := http2amqp.NewHttp2AmqpService(brokerUri, exchange, timeout)
-
-	http.HandleFunc("/", http2amqp.NewHTTPServerFunc(http2amqpService))
-	log.Println("[http2amqp] Starting HTTP server at 127.0.0.1:18080 ...")
-	http.ListenAndServe("127.0.0.1:18080", nil)
-
-}
-
-func parseArgs() (string, string, time.Duration) {
 	amqpuri := flag.String("amqpuri", localBrokerUri(), "AMQP connection uri")
+	address := flag.String("address", "0.0.0.0", "Listen address")
+	port := flag.String("port", "18080", "Listen port")
 	exchange := flag.String("exchange", "events", "AMQP exchange name")
 	timeout := flag.Int("timeout", 1000, "Queries timeout in milliseconds")
 	flag.Parse()
 
-	return *amqpuri, *exchange, time.Duration(*timeout) * time.Millisecond
+	http2amqpService := http2amqp.NewHttp2AmqpService(*amqpuri, *exchange, time.Duration(*timeout)*time.Millisecond)
+
+	http.HandleFunc("/", http2amqp.NewHTTPServerFunc(http2amqpService))
+	addressAndPort := fmt.Sprintf("%s:%s", *address, *port)
+	log.Println("[http2amqp] Starting HTTP server at ", addressAndPort)
+	http.ListenAndServe(addressAndPort, nil)
 }
 
 func localBrokerUri() string {
