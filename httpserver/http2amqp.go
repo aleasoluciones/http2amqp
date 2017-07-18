8192ca1f1fa3bf1dc170b5f3c63d08d1dcc4b478
Jorge Senín <jsenin@alea-soluciones.com>
2017-07-18 14:34:41 +0200

    Added verbose option at command line params

--- httpserver/http2amqp.go
+++ httpserver/http2amqp.go
@@ -17,12 +17,17 @@ func main() {
 	alealog.Init()
 	alealog.DisableLogging()
 
+	verbose := flag.Bool("verbose", false, "Verbose mode, enable logging")
 	amqpuri := flag.String("amqpuri", localBrokerUri(), "AMQP connection uri")
 	address := flag.String("address", "0.0.0.0", "Listen address")
 	port := flag.String("port", "18080", "Listen port")
 	exchange := flag.String("exchange", "events", "AMQP exchange name")
 	timeout := flag.Int("timeout", 1000, "Queries timeout in milliseconds")
 	flag.Parse()
+
+  if *verbose {
+    alealog.EnableLogging()
+  }
 
 	service := http2amqp.NewService(*amqpuri, *exchange, time.Duration(*timeout)*time.Millisecond)
 
