e6e73e8b434fe84b02825fa8dda9f500803fa953
Alberto Perez <aperez@alea-soluciones.com>
2018-01-30 16:51:34 +0100

    Updated build.sh for our IC

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ WORKDIR=/tmp/$$/$(hostname)
 rm -rf $WORKDIR
 mkdir -p ${WORKDIR}
 cp -a . ${WORKDIR}
-docker run --rm -v ${WORKDIR}:${SRC_DIR} -e CGO_ENABLED=0 -e GOOS=linux golang:${GO_VERSION} bash -c "cd ${SRC_DIR};make deps; make build"
+docker run --rm -v ${WORKDIR}:${SRC_DIR} -e CGO_ENABLED=0 -e GOOS=linux golang:${GO_VERSION} bash -c "cd ${SRC_DIR};make jenkins"
 cp -v ${WORKDIR}/http2amqp .
 cp -v ${WORKDIR}/echoservice .
 docker build --no-cache -t aleasoluciones/http2amqp .
